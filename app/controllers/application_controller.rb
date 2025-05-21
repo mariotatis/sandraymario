d9dd32df44fa7c5ab5ad743f894fcd2dad0eb282
Mario <mario@koombea.com>
2025-05-20 22:53:06 -0500

    Upgrade Ruby from 2.1.0 to 3.3.0 and Rails from 4.1.0 to 7.2.2.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # Skip CSRF protection for JS requests in development
+  skip_before_action :verify_authenticity_token, if: -> { request.format.js? && Rails.env.development? }
 end
